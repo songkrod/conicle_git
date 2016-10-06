6b70018d09b3f9a575ae586fdce8e830b0ab7965
Songkrod Thanavorn <songkrod.th@gmail.com>
2016-10-06 23:11:02 +0200

    push slack

--- public/modules/page/controller/contact.client.controller.js
+++ public/modules/page/controller/contact.client.controller.js
@@ -1,6 +1,27 @@
 angular.module('page').controller('ContactController', [
 	'$scope',
-	function ($scope) {
-		//
+	'$http',
+	function ($scope, $http) {
+		$scope.sendContact = function() {
+			var d = {
+				"channel": "#landing",
+				"text": "Contact From " + $scope.form.email,
+				"attachments" : [
+					{
+						"title": "Contact Information",
+						"text": "Name : " + $scope.form.name + "\nCompany : " + $scope.form.company + "\nMessage : " + $scope.form.msg
+					}
+				]
+			};
+
+			$http({
+				url: "https://hooks.slack.com/services/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",
+				data: d,
+				method: 'POST',
+				headers : {'Content-Type':'application/x-www-form-urlencoded; charset=UTF-8'}
+			}).success(function(data) {
+				console.log("OK", data);
+			}).error(function(err) {console.log("ERR", err)})
+		};
 	}
 ]);
